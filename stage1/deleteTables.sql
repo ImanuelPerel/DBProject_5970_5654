cb9ddda997f0e2197e35dfa810ea94d4a4f8a0f4
ImanuelPerel <153401643+ImanuelPerel@users.noreply.github.com>
2025-05-11 19:20:55 +0300

    Create deleteTables.sql

---
+++ stage1/deleteTables.sql
@@ -0,0 +1,8 @@
+DELETE FROM ordered;
+DELETE FROM service;
+DELETE FROM shiftforworker;
+DELETE FROM monthlyworker;
+DELETE FROM hourlyworker;
+DELETE FROM customer;
+DELETE FROM employee;
+DELETE FROM person;
